da29e202dd4e153f979257ee1825a2d053512aed
boost1ne <boustane.chaimaa@gmail.com>
2024-05-19 12:54:50 -0700

    v1

--- console.py
+++ console.py
@@ -5,22 +5,28 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 
-    """documentation"""
+    """HBNB command interpreter class."""
 
     prompt = '(hbnb) '
 
     def do_EOF(self, line):
+        """Handle EOF to exit the program"""
+        print()
         return True
 
-    do_quit = do_EOF
+    def do_quit(self, line):
+       """Quits the command interpreter."""
+       return True
 
     def emptyline(self):
+        """Ignores empty lines."""
         pass
 
     def help_EOF(self):
-        print('End Of File to close the console')
+        print('End Of File (Ctrl-D) to close the console')
 
     def help_quit(self):
+        """Print help for the EOF command."""
         print('Quit command to exit the program')
 
 
