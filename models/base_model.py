06583d0c97d6dab23efede60c373309a984460e2
boost1ne <boustane.chaimaa@gmail.com>
2024-05-20 14:27:27 -0700

    update BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -12,6 +12,9 @@ import models
 class BaseModel:
     def __init__(self, *args, **kwargs):
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -20,11 +23,6 @@ class BaseModel:
                     setattr(self, key, datetime.strptime(value, time_format))
                 else:
                     setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
         models.storage.new(self)
     def __str__(self):
         """
@@ -48,4 +46,17 @@ class BaseModel:
         inst_dict["updated_at"] = self.updated_at.isoformat()
         inst_dict["__class__"] = self.__class__.__name__
         return inst_dict
+
+    if __name__ == "__main__":
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
 
