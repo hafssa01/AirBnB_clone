b1403835c7bb13004292cab753db6a8e13562fa0
boost1ne <boustane.chaimaa@gmail.com>
2024-05-20 15:28:02 -0700

    save

--- models/base_model.py
+++ models/base_model.py
@@ -37,6 +37,7 @@ class BaseModel:
         Updates the updated_at attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
