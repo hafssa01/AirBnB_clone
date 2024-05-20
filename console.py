6498abfc27df806c06fda2dcbb8012187728b651
boost1ne <boustane.chaimaa@gmail.com>
2024-05-20 02:36:23 +0200

    console v2

--- console.py
+++ console.py
@@ -5,13 +5,15 @@
 This script provides a simple command-line interface for interacting with
 the HBNB application (if applicable). It allows users to execute commands
 by typing them at the prompt.
+And this is a list of all used modules in this section
 """
 
 import cmd
-
+from models.base_model import BaseModel 
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
-
+    
     """HBNB command interpreter class.
 
     This class provides a simple command-line interface for interacting
@@ -19,6 +21,7 @@ class HBNBCommand(cmd.Cmd):
     """
 
     prompt = '(hbnb) '
+    classList = ["BaseModel"]
 
     def do_EOF(self, line):
         """Handle EOF to exit the program."""
@@ -39,6 +42,132 @@ class HBNBCommand(cmd.Cmd):
     def help_quit(self):
         """Print help for the EOF command."""
         print('Quit command to exit the program')
+
+    #CRUD operations
+
+    def do_create(self, line):
+        """
+        create: Creates a new instance of BaseModel, 
+        Saves it (to the JSON file) and prints the unique id
+        """
+        args = line.split()
+        if len(args) == 0:
+            print('** class name missing **')
+        elif len(args) == 1:
+            if args[0] not in self.classList:
+                print("** class doesn't exist **")
+            else:
+                new_instance = BaseModel()
+                new_instance.save()
+                print(new_instance.id)
+
+    def do_show(self, line):
+        """
+        Prints the string representation of an instance
+        based on the class name and id
+        """
+        args = line.split()
+        if len(args) == 0:
+            print('** class name missing **')
+        elif args[0] not in self.classList:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("*** instance id is missing ***")
+        else:
+            dict = storage.all() #retrieves all objects from the storage
+
+            key = "{}.{}".format(args[0], args[1]) #constructed key using string formatting
+            if key in dict: #checks if instance exists in returned dict
+                print(dict[key])
+            else:
+                print("*** no instance found ***")
+
+    def do_destroy(self, line):
+        """
+         Deletes an instance based on the class name and id
+         Saves the change into the JSON file).
+        """
+
+        args = line.split()
+        if len(args) == 0:
+            print('** class name missing **')
+        elif args[0] not in self.classList:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("*** instance id is missing ***")
+        else:
+            dict = storage.all() #retrieves all objects from the storage
+
+            key = "{}.{}".format(args[0], args[1]) #constructed key using string formatting
+            if key in dict: #checks if instance exists in returned dict
+                del dict[key]
+                storage.save()
+            else:
+                print("*** no instance found ***")
+    
+    def do_all(self, line):
+        """
+        Prints all string representation of all instances based or not on the class name
+        """
+        dict = storage.all() #retrieves all objects from the storage
+        args = line.split()
+
+        if len(args) == 0:
+            for key, value in dict.items():
+                print(str(value))
+        elif args[0] not in self.classList:
+            print("** class doesn't exist **")
+        else:
+            for key, value in dict.items():
+                if key.split('.')[0] == args[0]:
+                    print(str(value))
+
+
+
+    def do_update(self, line):
+        """
+        Updates an instance based on the class name and id
+        By adding or updating attribute (save the change into the JSON file)
+        """
+
+        args = line.split()
+
+        if len(args) == 0:
+            print('** class name missing **')
+        elif args[0] not in self.classList:
+            print("** class doesn't exist **")
+        elif len(args) < 2:
+            print("** instance id is missing **")
+        else:
+            dict = storage.all() #retrieves all objects from the storage
+
+            key = "{}.{}".format(args[0], args[1]) #constructed key using string formatting
+            if key not in dict: #checks if instance exists in returned dict
+                print("** no instance found **")
+            elif len(args) < 3:
+                print("** attribute name missing **")
+            elif len(args) < 4:
+                print("** value missing **")
+            else:
+                #retrieves an object associated to
+                #specefic class name w/ an unique id
+                obj = dict[key] 
+
+                att_name = args[2]
+                att_value = args[3]
+
+                #Handles type's error
+                try:
+                    att_value = eval(att_value) #evaluating this string val
+                except Exception:
+                    pass
+                #Updates the object's attribute
+                #to a new given value
+                setattr(obj, att_name, att_value)
+
+                obj.save()
+  
+
 
 
 if __name__ == '__main__':
